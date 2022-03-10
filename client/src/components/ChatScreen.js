205145449480425a5243f84981e4c2fb8d4dd4a6
zyar3k <a.zygan@gmail.com>
2022-03-10 14:09:48 +0100

    Add chat screen styles

--- client/src/components/ChatScreen.js
+++ client/src/components/ChatScreen.js
@@ -1,12 +1,48 @@
+import {
+  AppBar,
+  Avatar,
+  Box,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { useParams } from "react-router-dom";
+import MessageCard from "./MessageCard";
 
 const ChatScreen = () => {
   const { id, name } = useParams();
   return (
-    <div>
-      {id} {name}
-    </div>
+    <Box flexGrow={1}>
+      <AppBar position="static" sx={{ backgroundColor: "white", boxShadow: 0 }}>
+        <Toolbar>
+          <Avatar
+            src={`https://avatars.dicebear.com/api/initials/${name}.svg`}
+            sx={{ width: "32px", height: "32px", mr: 2 }}
+          />
+          <Typography variant="h6" color="black">
+            {name}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        backgroundColor="#f5f5f5"
+        height="80vh"
+        padding="10px"
+        sx={{ overflowY: "auto" }}
+      >
+        <MessageCard text="Hello World!" date="1212" direction="end" />
+        <MessageCard text="Hello World!" date="1212" direction="start" />
+        <MessageCard text="Hello World!" date="1212" direction="end" />
+      </Box>
+      <TextField
+        placeholder="Type a message"
+        variant="standard"
+        fullWidth
+        multiline
+        rows={2}
+      />
+    </Box>
   );
 };
 
---
+++ client/src/components/MessageCard.js
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
+const MessageCard = ({ text, date, direction }) => {
+  return (
+    <Box display="flex" justifyContent={direction}>
+      <Box>
+        <Typography variant="subtitle2" backgroundColor="white" padding="5px">
+          {text}
+        </Typography>
+        <Typography variant="caption">{date}</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default MessageCard;
