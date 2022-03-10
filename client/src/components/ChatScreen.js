cff35ad4f256243fdda55e350277ea18a96f98b7
zyar3k <a.zygan@gmail.com>
2022-03-10 16:22:35 +0100

    Add fetch messages

--- client/src/components/ChatScreen.js
+++ client/src/components/ChatScreen.js
@@ -6,12 +6,50 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import MessageCard from "./MessageCard";
 
 const ChatScreen = () => {
   const { id, name } = useParams();
+
+  const [messages, setMessages] = useState([]);
+
+  const getAllMessages = async () => {
+    fetch(`http://localhost:4000/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjYsImlhdCI6MTY0NjkyNTEzMiwiZXhwIjoxNjQ2OTI4NzMyfQ.Di3fexy0tXdmSHC3IDJKYttDFN_C_kyJKZ8oum04jNc",
+      },
+      body: JSON.stringify({
+        query: `
+          query MessagesByUser($receiverId: Int!) {
+            messagesByUser(receiverId: $receiverId) {
+              id
+              text
+              receiverId
+              senderId
+              createdAt
+            }
+          }
+        `,
+        variables: {
+          receiverId: 2,
+        },
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  useEffect(() => {
+    getAllMessages();
+  }, []);
+
   return (
     <Box flexGrow={1}>
       <AppBar position="static" sx={{ backgroundColor: "white", boxShadow: 0 }}>
