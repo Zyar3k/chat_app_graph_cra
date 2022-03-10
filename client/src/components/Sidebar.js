5715712c72c3eb4914ab03fa54696e75485aa772
zyar3k <a.zygan@gmail.com>
2022-03-10 18:49:35 +0100

    Attach token, users from db

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Box, Typography, Divider, Stack } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
 import UserCard from "./UserCard";
+import { useQuery } from "@apollo/client";
+import { GET_ALL_USERS } from "../graphql/queries";
 
 const Sidebar = ({ setLoggedIn }) => {
-  const users = [
-    { id: 1, firstName: "John", lastName: "Doe" },
-    { id: 2, firstName: "Jane", lastName: "Doe" },
-    { id: 3, firstName: "John", lastName: "Smith" },
-  ];
+  const { loading, data, error } = useQuery(GET_ALL_USERS);
+
+  if (loading) return <Typography variant="h6">Loading chats...</Typography>;
+  if (data) console.log(data);
+  if (error) console.error(error.message);
 
   return (
     <Box backgroundColor="#f7f7f7" height="100vh" width="250px" padding="10px">
@@ -22,7 +24,7 @@ const Sidebar = ({ setLoggedIn }) => {
         />
       </Stack>
       <Divider />
-      {users.map((user, index) => {
+      {data.users.map((user, index) => {
         return <UserCard key={index} user={user} />;
       })}
     </Box>
--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,30 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("jwt");
+  return {
+    headers: {
+      ...headers,
+      authorization: token || "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
